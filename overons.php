4d2a1f9f8a575cf63ce594a11775d3c7998e2133
GlennWal <Glenn.vanderwal@student.rocva.nl>
2024-05-23 16:35:52 +0200

    Codes update

--- overons.php
+++ overons.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Adventu</title>
-    <link rel="stylesheet" type="text/css" href="css/contact.css">
+    <link rel="stylesheet" type="text/css" href="css/overons.css">
 
     <!-- box-icons link -->
     <link rel="stylesheet"
@@ -21,9 +21,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
 </head>
 <body>
-
 
 <!--- header --->
 <header>
@@ -33,19 +33,64 @@
 
 </header>
 
-<!--- Homepagina --->
+<!--- home --->
 <section class="home">
     <div class="home-text" data-aos="fade-up">
-        <h6>Beschikbaar van 8:00 t/m 17:00</h6>
-        <h1>Contacteer ons! <br> ready to talk!</h1>
-        <p>"Klaar om jouw vragen te beantwoorden! Neem contact op en ervaar onze toegewijde klantenservice."</p>
-        <div>
-            <a href="mailto:glennmah@live.nl" class="btn">Email ons!  <i class="ri-arrow-right-line"></i></a>
-            <a href="javascript:void(0)" class="btn" onclick="openPopup()">Bel ons! <i class="ri-arrow-right-line"></i> </a>
+        <h6>New Arrivals</h6>
+        <h1>Nu gearriveerd!<br>ready to rock</h1>
+        <p>"Ontdek onze nieuwste aanwinst: nu gearriveerd en klaar om jou te laten schitteren!"</p>
 
-        </div>
     </div>
 </section>
+
+<?php
+require_once "database/conn.php";
+
+$sql = "SELECT * FROM leden";
+?>
+
+<div class="team-member">
+    <!--        </div>-->
+    <?php
+    if ($result = $conn->query($sql)) {
+        while ($row = $result->fetch_row()) {
+            $voornaam = $row[1];
+            $tussenvoegsel = $row[2];
+            $achternaam = $row[3];
+            $email = $row[4];
+            $bio = $row[5]
+
+
+            ?>
+
+            <div class="info">
+                <img src="./img/n2.png" alt="Foto">
+                <h5><?php echo $voornaam; ?> <?php echo $tussenvoegsel; ?> <?php echo $achternaam; ?></h5>
+
+
+                <!--                <button onclick="showInfo(this)">Meer informatie</button>-->
+                <p><?php echo $email; ?></p>
+                <p><?php echo $bio; ?></p>
+
+                <ul class="social-icons">
+                    <li><a href="https://www.linkedin.com/janesmith" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                    </li>
+                    <li><a href="https://www.twitter.com/janesmith" target="_blank"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <!-- Voeg hier meer social media links toe -->
+                </ul>
+            </div>
+
+
+            <?php
+        }
+    }
+    ?>
+
+</div>
+<!-- Voeg hier meer teamleden toe -->
+
+</div>
 
 
 <!--- footer --->
@@ -54,8 +99,7 @@
         <h3>Het bedrijf</h3>
         <a href="overons.php">Over ons</a>
         <a href="#">Winkel</a>
-        <a href="contact.php">Contact</a>
-        <a href="privacyverklaring.php">privacy verklaring</a>
+        <a href="#">Contact</a>
     </div>
 
 
@@ -88,4 +132,15 @@
         offset: 300,
         duration: 1450,
     });
+
+    function showInfo(member) {
+        var info = member.nextElementSibling;
+        if (info.style.display === "none") {
+            info.style.display = "block";
+        } else {
+            info.style.display = "none";
+        }
+    }
+
 </script>
+
